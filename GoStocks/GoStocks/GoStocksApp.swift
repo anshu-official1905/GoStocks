a4b77e3aa85ada1342c23640b205c0d4b3a9a66a
anshu-official1905 <57668446+anshu-official1905@users.noreply.github.com>
2023-12-13 19:43:56 +0530

    Added Model for Stock which adheres to SwiftData (#2)

--- GoStocks/GoStocks/GoStocksApp.swift
+++ GoStocks/GoStocks/GoStocksApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct GoStocksApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StocksListView()
         }
+        .modelContainer(for: Stocks.self)
     }
 }
