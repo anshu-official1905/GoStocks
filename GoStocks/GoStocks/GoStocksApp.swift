327fe98741f80227fbcfdd766a54adb5712bfd78
anshu-official1905 <57668446+anshu-official1905@users.noreply.github.com>
2023-12-13 17:56:38 +0530

    Initial setup of GoStocks Project (#1)
    * Setup initial project
    
    * Initial project setup
    
    * Initial project setup
    
    ---------
    
    Co-authored-by: anshu <>

---
+++ GoStocks/GoStocks/GoStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoStocksApp.swift
+//  GoStocks
+//
+//  Created by Anshu Kumar Ray on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GoStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
